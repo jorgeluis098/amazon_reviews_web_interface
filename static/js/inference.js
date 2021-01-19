6068a1bcd01a69501d06a869c09d9c6ae11a715d
jorgeluis098 <jorge.luis.rb.98@gmail.com>
2021-01-19 00:41:35 -0600

    fix pull request

--- static/js/inference.js
+++ static/js/inference.js
@@ -23,32 +23,24 @@ $(document).ready(function (){
         }); 
     });
     graphs();
-    function graphs (){
+    function graphs (data){
             var ctx = document.getElementById('myChart');
             var myChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: ['Red', 'Blue'],
+                labels: ['Negativos', 'Positivos'],
                 datasets: [{
-                    label: '# of Votes',
+                    label: 'Numero de comentarios',
                     data: [12, 19],
                     backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
+                        'rgba(255, 99, 132, 0.3)',
+                        'rgba(25, 135, 84, 0.3)'
                     ],
                     borderColor: [
                         'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
+                        'rgba(25, 135, 84, 1)'
                     ],
-                    borderWidth: 1
+                    borderWidth: 0.5
                 }]
             },
             options: {
